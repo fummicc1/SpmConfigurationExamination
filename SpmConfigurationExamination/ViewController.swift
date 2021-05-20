e6e3fe2ca303b8f29fed71756bf51b575301ecaf
fummicc1 <fumiya.tennis1@gmail.com>
2021-05-20 21:19:35 +0900

    initial state.

--- SpmConfigurationExamination/ViewController.swift
+++ SpmConfigurationExamination/ViewController.swift
@@ -6,16 +6,11 @@
 //
 
 import UIKit
-import EasyFirebaseSwift
-import FirebaseFirestore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
