c63714e0902d8237a0826b1c26507b6af723fc11
fummicc1 <fumiya.tennis1@gmail.com>
2021-05-20 16:18:11 +0900

    first commit

---
+++ SpmConfigurationExamination/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SpmConfigurationExamination
+//
+//  Created by Fumiya Tanaka on 2021/05/20.
+//
+
+import UIKit
+import EasyFirebaseSwift
+import FirebaseFirestore
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
